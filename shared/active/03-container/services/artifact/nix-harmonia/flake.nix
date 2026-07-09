3acdcb52caf680f692d363c6ce0516d3037d66ee
levonk <277861+levonk@users.noreply.github.com>
2026-07-08 22:49:24 -0700

    fix(nix-harmonia): support aarch64-linux for cross-arch builds
    The flake previously hardcoded x86_64-linux only, preventing builds on
    aarch64-linux hosts (e.g., OCI cloud server). The mold linker segfaults
    under QEMU x86_64 binfmt emulation on aarch64, so native builds are
    required. Added aarch64-linux to supportedSystems using forAllSystems
    pattern. Harmonia upstream already supports aarch64-linux.

--- shared/active/03-container/services/artifact/nix-harmonia/flake.nix
+++ shared/active/03-container/services/artifact/nix-harmonia/flake.nix
@@ -9,105 +9,112 @@
 
   outputs = { self, nixpkgs, harmonia }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        # Add binary cache for faster downloads
-        config = {
-          allowUnfree = false;
-          substituters = [
-            "https://cache.nixos.org"
-            "https://nix-community.cachix.org"
-            "https://cache.garnix.io"
-          ];
-          trusted-public-keys = [
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPSQZNGZfdL7Q="
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CXHQrkxhLww6X236k="
-            "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr3g="
-          ];
-        };
-      };
-      harmoniaPkg = harmonia.packages.${system}.default;
-
-      commonConfig = {
-        Entrypoint = [ "${harmoniaPkg}/bin/harmonia" ];
-        ExposedPorts = {
-          "5000/tcp" = {};
-        };
-        WorkingDir = "/data";
-        Volumes = {
-          "/data" = {};
-          "/nix/store" = {};
-        };
-        Env = [
-          "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-        ];
-      };
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = fn: builtins.listToAttrs (builtins.map (system: {
+        name = system;
+        value = fn system;
+      }) supportedSystems);
     in
     {
-      packages.${system} = {
-        default = harmoniaPkg;
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            # Add binary cache for faster downloads
+            config = {
+              allowUnfree = false;
+              substituters = [
+                "https://cache.nixos.org"
+                "https://nix-community.cachix.org"
+                "https://cache.garnix.io"
+              ];
+              trusted-public-keys = [
+                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPSQZNGZfdL7Q="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CXHQrkxhLww6X236k="
+                "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr3g="
+              ];
+            };
+          };
+          harmoniaPkg = harmonia.packages.${system}.default;
 
-        docker-prod = pkgs.dockerTools.buildLayeredImage {
-          name = "harmonia";
-          tag = "latest";
-          created = "now";
-          contents = [
-            harmoniaPkg
-            pkgs.bash
-            pkgs.coreutils
-            pkgs.nix
-            pkgs.iana-etc
-            pkgs.cacert
-          ];
-          config = commonConfig;
-        };
-
-        docker-debug = pkgs.dockerTools.buildLayeredImage {
-          name = "harmonia-debug";
-          tag = "latest";
-          created = "now";
-          contents = [
-            harmoniaPkg
-            pkgs.nix
-            pkgs.bashInteractive
-            pkgs.coreutils
-            # Debug tools
-            pkgs.zsh
-            pkgs.curl
-            pkgs.wget
-            pkgs.iproute2
-            pkgs.dnsutils
-            pkgs.netcat-gnu
-            pkgs.tcpdump
-            pkgs.socat
-            pkgs.mtr
-            pkgs.iputils
-            pkgs.procps
-            pkgs.strace
-            pkgs.lsof
-            pkgs.htop
-            pkgs.psmisc
-            pkgs.vim
-            pkgs.jq
-            pkgs.ripgrep
-            pkgs.findutils
-            pkgs.file
-            pkgs.tree
-            pkgs.gnused
-            pkgs.gnugrep
-            pkgs.gawk
-            pkgs.less
-            pkgs.which
-            pkgs.iana-etc
-            pkgs.cacert
-          ];
-          config = commonConfig // {
-            Env = commonConfig.Env ++ [
-              "PATH=/bin:${pkgs.zsh}/bin:${pkgs.bashInteractive}/bin:${pkgs.coreutils}/bin:${pkgs.curl}/bin:${pkgs.procps}/bin:${pkgs.iproute2}/bin:${pkgs.jq}/bin:${pkgs.vim}/bin"
+          commonConfig = {
+            Entrypoint = [ "${harmoniaPkg}/bin/harmonia" ];
+            ExposedPorts = {
+              "5000/tcp" = {};
+            };
+            WorkingDir = "/data";
+            Volumes = {
+              "/data" = {};
+              "/nix/store" = {};
+            };
+            Env = [
+              "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
             ];
           };
-        };
-      };
+        in
+        {
+          default = harmoniaPkg;
+
+          docker-prod = pkgs.dockerTools.buildLayeredImage {
+            name = "harmonia";
+            tag = "latest";
+            created = "now";
+            contents = [
+              harmoniaPkg
+              pkgs.bash
+              pkgs.coreutils
+              pkgs.nix
+              pkgs.iana-etc
+              pkgs.cacert
+            ];
+            config = commonConfig;
+          };
+
+          docker-debug = pkgs.dockerTools.buildLayeredImage {
+            name = "harmonia-debug";
+            tag = "latest";
+            created = "now";
+            contents = [
+              harmoniaPkg
+              pkgs.nix
+              pkgs.bashInteractive
+              pkgs.coreutils
+              # Debug tools
+              pkgs.zsh
+              pkgs.curl
+              pkgs.wget
+              pkgs.iproute2
+              pkgs.dnsutils
+              pkgs.netcat-gnu
+              pkgs.tcpdump
+              pkgs.socat
+              pkgs.mtr
+              pkgs.iputils
+              pkgs.procps
+              pkgs.strace
+              pkgs.lsof
+              pkgs.htop
+              pkgs.psmisc
+              pkgs.vim
+              pkgs.jq
+              pkgs.ripgrep
+              pkgs.findutils
+              pkgs.file
+              pkgs.tree
+              pkgs.gnused
+              pkgs.gnugrep
+              pkgs.gawk
+              pkgs.less
+              pkgs.which
+              pkgs.iana-etc
+              pkgs.cacert
+            ];
+            config = commonConfig // {
+              Env = commonConfig.Env ++ [
+                "PATH=/bin:${pkgs.zsh}/bin:${pkgs.bashInteractive}/bin:${pkgs.coreutils}/bin:${pkgs.curl}/bin:${pkgs.procps}/bin:${pkgs.iproute2}/bin:${pkgs.jq}/bin:${pkgs.vim}/bin"
+              ];
+            };
+          };
+        });
     };
 }
